55d300029332026a830951f2b4808428d3ad45e9
patrick brady <patrick.brady@radar-apps.com>
2025-06-10 12:39:34 -0400

    added test for wideband reader

--- six/modules/c++/crsd/unittests/test_read_wideband.cpp
+++ six/modules/c++/crsd/unittests/test_read_wideband.cpp
@@ -1,10 +1,10 @@
 /* =========================================================================
- * This file is part of cphd-c++
+ * This file is part of crsd-c++
  * =========================================================================
  *
  * (C) Copyright 2004 - 2020, MDA Information Systems LLC
  *
- * cphd-c++ is free software; you can redistribute it and/or modify
+ * crsd-c++ is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
@@ -19,9 +19,9 @@
  * see <http://www.gnu.org/licenses/>.
  *
  */
-#include <cphd/Wideband.h>
+#include <crsd/Wideband.h>
 
-#include <cphd/Metadata.h>
+#include <crsd/Metadata.h>
 #include <io/ByteStream.h>
 #include "TestCase.h"
 
@@ -31,15 +31,17 @@ TEST_CASE(testReadCompressedChannel)
     input->write("1234");
     input->seek(0, io::Seekable::START);
 
-    cphd::Metadata metadata;
-    metadata.data.channels.resize(1);
-    metadata.data.channels[0].numSamples = 4;
-    metadata.data.channels[0].numVectors = 8;
-    metadata.data.signalArrayFormat = cphd::SignalArrayFormat::CI2;
-    metadata.data.signalCompressionID = "Test Compression";
-    metadata.data.channels[0].compressedSignalSize = 4;
+    crsd::Metadata metadata;
+    metadata.data.receiveParameters.reset(new crsd::Data::Receive());
+    metadata.data.receiveParameters->channels.resize(1);
+    metadata.data.receiveParameters->channels[0].numSamples = 4;
+    metadata.data.receiveParameters->channels[0].numVectors = 8;
+    metadata.data.receiveParameters->signalArrayFormat = crsd::SignalArrayFormat::CI2;
+    metadata.data.receiveParameters->signalCompression.reset(new crsd::Data::SignalCompression());
+    metadata.data.receiveParameters->signalCompression->identifier = "Test Compression";
+    metadata.data.receiveParameters->channels[0].compressedSignalSize = 4;
 
-    cphd::Wideband wideband(input, metadata, 0, 4);
+    crsd::Wideband wideband(input, metadata, 0, 4);
 
     std::unique_ptr<std::byte[]> readData;
     wideband.read(0, readData);
@@ -57,15 +59,16 @@ TEST_CASE(testReadUncompressedChannel)
     input->write("12345678");
     input->seek(0, io::Seekable::START);
 
-    cphd::Metadata metadata;
-    metadata.data.channels.resize(1);
-    metadata.data.channels[0].numSamples = 1;
-    metadata.data.channels[0].numVectors = 4;
-    metadata.data.signalArrayFormat = cphd::SignalArrayFormat::CI2;
-    metadata.data.channels[0].compressedSignalSize =
+    crsd::Metadata metadata;
+    metadata.data.receiveParameters.reset(new crsd::Data::Receive());
+    metadata.data.receiveParameters->channels.resize(1);
+    metadata.data.receiveParameters->channels[0].numSamples = 1;
+    metadata.data.receiveParameters->channels[0].numVectors = 4;
+    metadata.data.receiveParameters->signalArrayFormat = crsd::SignalArrayFormat::CI2;
+    metadata.data.receiveParameters->channels[0].compressedSignalSize =
             six::Init::undefined<size_t>();
 
-    cphd::Wideband wideband(input, metadata, 0, 4);
+    crsd::Wideband wideband(input, metadata, 0, 4);
 
     std::unique_ptr<std::byte[]> readData;
     wideband.read(0, readData);
@@ -79,11 +82,12 @@ TEST_CASE(testReadUncompressedChannel)
 
 TEST_CASE(testReadChannelSubset)
 {
-    cphd::Metadata metadata;
-    metadata.data.channels.resize(1);
-    metadata.data.channels[0].numSamples = 2;
-    metadata.data.channels[0].numVectors = 4;
-    metadata.data.signalArrayFormat = cphd::SignalArrayFormat::CI2;
+    crsd::Metadata metadata;
+    metadata.data.receiveParameters.reset(new crsd::Data::Receive());
+    metadata.data.receiveParameters->channels.resize(1);
+    metadata.data.receiveParameters->channels[0].numSamples = 2;
+    metadata.data.receiveParameters->channels[0].numVectors = 4;
+    metadata.data.receiveParameters->signalArrayFormat = crsd::SignalArrayFormat::CI2;
 
     auto input = std::make_shared<io::ByteStream>();
     input->write("0A1B");
@@ -92,7 +96,7 @@ TEST_CASE(testReadChannelSubset)
     input->write("6G7H");
     input->seek(0, io::Seekable::START);
 
-    cphd::Wideband wideband(input, metadata, 0, 32);
+    crsd::Wideband wideband(input, metadata, 0, 32);
 
     // Single pixel reads
     auto readData = wideband.read(0, 0, 0, 0, 0, 1);
@@ -110,15 +114,15 @@ TEST_CASE(testReadChannelSubset)
     TEST_ASSERT_EQ(readData[1], static_cast<std::byte>('H'));
 
     // Full vector reads
-    readData = wideband.read(0, 0, 0, 0, cphd::Wideband::ALL, 1);
+    readData = wideband.read(0, 0, 0, 0, crsd::Wideband::ALL, 1);
     TEST_ASSERT_EQ(readData[0], static_cast<std::byte>('0'));
     TEST_ASSERT_EQ(readData[1], static_cast<std::byte>('A'));
     TEST_ASSERT_EQ(readData[2], static_cast<std::byte>('1'));
     TEST_ASSERT_EQ(readData[3], static_cast<std::byte>('B'));
 
-    readData = wideband.read(0, 3, 3, 0, cphd::Wideband::ALL, 1);
+    readData = wideband.read(0, 3, 3, 0, crsd::Wideband::ALL, 1);
     TEST_ASSERT_EQ(
-            wideband.getBytesRequiredForRead(0, 3, 3, 0, cphd::Wideband::ALL),
+            wideband.getBytesRequiredForRead(0, 3, 3, 0, crsd::Wideband::ALL),
         static_cast<size_t>(4));
 
     TEST_ASSERT_EQ(readData[0], static_cast<std::byte>('6'));
@@ -128,9 +132,9 @@ TEST_CASE(testReadChannelSubset)
 
     // Full sample reads
     TEST_ASSERT_EQ(
-            wideband.getBytesRequiredForRead(0, 0, cphd::Wideband::ALL, 0, 0),
+            wideband.getBytesRequiredForRead(0, 0, crsd::Wideband::ALL, 0, 0),
         static_cast<size_t>(8));
-    readData = wideband.read(0, 0, cphd::Wideband::ALL, 0, 0, 1);
+    readData = wideband.read(0, 0, crsd::Wideband::ALL, 0, 0, 1);
     TEST_ASSERT_EQ(readData[0], static_cast<std::byte>('0'));
     TEST_ASSERT_EQ(readData[1], static_cast<std::byte>('A'));
     TEST_ASSERT_EQ(readData[2], static_cast<std::byte>('2'));
@@ -147,15 +151,17 @@ TEST_CASE(testCannotDoPartialReadOfCompressedChannel)
     input->write("1234");
     input->seek(0, io::Seekable::START);
 
-    cphd::Metadata metadata;
-    metadata.data.channels.resize(1);
-    metadata.data.channels[0].numSamples = 4;
-    metadata.data.channels[0].numVectors = 8;
-    metadata.data.signalArrayFormat = cphd::SignalArrayFormat::CI2;
-    metadata.data.signalCompressionID = "Test Compression";
-    metadata.data.channels[0].compressedSignalSize = 4;
+    crsd::Metadata metadata;
+    metadata.data.receiveParameters.reset(new crsd::Data::Receive());
+    metadata.data.receiveParameters->channels.resize(1);
+    metadata.data.receiveParameters->channels[0].numSamples = 4;
+    metadata.data.receiveParameters->channels[0].numVectors = 8;
+    metadata.data.receiveParameters->signalArrayFormat = crsd::SignalArrayFormat::CI2;
+    metadata.data.receiveParameters->signalCompression.reset(new crsd::Data::SignalCompression());
+    metadata.data.receiveParameters->signalCompression->identifier = "Test Compression";
+    metadata.data.receiveParameters->channels[0].compressedSignalSize = 4;
 
-    cphd::Wideband wideband(input, metadata, 0, 4);
+    crsd::Wideband wideband(input, metadata, 0, 4);
 
     TEST_EXCEPTION(wideband.read(0, 0, 0, 1, 1, 1));
     TEST_EXCEPTION(wideband.getBytesRequiredForRead(0, 0, 0, 1, 1));

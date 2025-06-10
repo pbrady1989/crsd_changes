c824beebffef4f07efaaf5c9f5c3f8b73ee1308d
patrick brady <patrick.brady@radar-apps.com>
2025-06-10 12:31:31 -0400

    test for pvp

--- six/modules/c++/crsd/unittests/test_pvp.cpp
+++ six/modules/c++/crsd/unittests/test_pvp.cpp
@@ -1,10 +1,10 @@
 /* =========================================================================
- * This file is part of cphd-c++
+ * This file is part of crsd-c++
  * =========================================================================
  *
  * (C) Copyright 2004 - 2019, MDA Information Systems LLC
  *
- * cphd-c++ is free software; you can redistribute it and/or modify
+ * crsd-c++ is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
@@ -25,46 +25,46 @@
 #include <xml/lite/MinidomParser.h>
 #include <six/Init.h>
 
-#include <cphd/PVP.h>
+#include <crsd/PVP.h>
 
 #include "TestCase.h"
 
 
 TEST_CASE(testSimpleEqualityOperatorTrue)
 {
-    cphd::Pvp pvp1, pvp2;
+    crsd::Pvp pvp1, pvp2;
 
-    pvp1.txTime.setOffset(0);
-    pvp2.txTime.setOffset(0);
+    pvp1.rcvStart.setOffset(0);
+    pvp2.rcvStart.setOffset(0);
 
     TEST_ASSERT_TRUE((pvp1 == pvp2));
 }
 
 TEST_CASE(testAppend)
 {
-    cphd::Pvp pvp;
-    pvp.append(pvp.txTime);
-    pvp.append(pvp.txPos);
-    pvp.append(pvp.txVel);
+    crsd::Pvp pvp;
+    pvp.append(pvp.rcvStart);
+    pvp.append(pvp.rcvPos);
+    pvp.append(pvp.rcvVel);
     pvp.append(pvp.ampSF);
     pvp.appendCustomParameter(8, "S8", "AddedParam1");
     pvp.append(pvp.signal);
 
-    TEST_ASSERT_TRUE(pvp.txTime.getOffset() == 0);
-    TEST_ASSERT_TRUE(pvp.txPos.getOffset() == 1);
-    TEST_ASSERT_TRUE(pvp.txVel.getOffset() == 4);
-    TEST_ASSERT_TRUE(pvp.ampSF.getOffset() == 7);
-    TEST_ASSERT_TRUE(pvp.addedPVP["AddedParam1"].getOffset() == 8);
-    TEST_ASSERT_TRUE(pvp.signal.getOffset() == 16);
+    TEST_ASSERT_TRUE(pvp.rcvStart.getOffset() == 0);
+    TEST_ASSERT_TRUE(pvp.rcvPos.getOffset() == 2);
+    TEST_ASSERT_TRUE(pvp.rcvVel.getOffset() == 5);
+    TEST_ASSERT_TRUE(pvp.ampSF.getOffset() == 8);
+    TEST_ASSERT_TRUE(pvp.addedPVP["AddedParam1"].getOffset() == 9);
+    TEST_ASSERT_TRUE(pvp.signal.getOffset() == 17);
 }
 
 TEST_CASE(testAddedParamsEqualityOperatorTrue)
 {
-    cphd::Pvp pvp1;
+    crsd::Pvp pvp1;
     pvp1.setCustomParameter(1, 0, "F8", "AddedParam1");
     pvp1.setCustomParameter(1, 1, "F8", "AddedParam2");
 
-    cphd::Pvp pvp2;
+    crsd::Pvp pvp2;
     pvp2.setCustomParameter(1, 0, "F8", "AddedParam1");
     pvp2.setCustomParameter(1, 1, "F8", "AddedParam2");
 
@@ -74,11 +74,11 @@ TEST_CASE(testAddedParamsEqualityOperatorTrue)
 
 TEST_CASE(testSimpleEqualityOperatorFalse)
 {
-    cphd::Pvp pvp1;
-    pvp1.fxN1.setOffset(0);
+    crsd::Pvp pvp1;
+    pvp1.frcv1.setOffset(0);
 
-    cphd::Pvp pvp2;
-    pvp2.txTime.setOffset(1);
+    crsd::Pvp pvp2;
+    pvp2.rcvStart.setOffset(1);
 
     TEST_ASSERT_TRUE((pvp1 != pvp2));
 
@@ -86,14 +86,14 @@ TEST_CASE(testSimpleEqualityOperatorFalse)
 
 TEST_CASE(testAddedParamsEqualityOperatorFalse)
 {
-    cphd::Pvp pvp1;
+    crsd::Pvp pvp1;
     pvp1.setCustomParameter(1, 0, "F8", "AddedParam1");
     pvp1.setCustomParameter(1, 1, "F8", "AddedParam2");
 
-    cphd::Pvp pvp2;
+    crsd::Pvp pvp2;
     pvp2.setCustomParameter(1, 0, "F8", "AddedParam1");
 
-    cphd::Pvp pvp3;
+    crsd::Pvp pvp3;
     pvp3.setCustomParameter(1, 0, "F8", "AddedParam1");
     pvp3.setCustomParameter(1, 1, "F8", "AddedParam3");
 
